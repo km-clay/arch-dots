c9ab7d12574fccec01545c19d7573541814c1bae
Kyler Clay <kylerclay@proton.me>
2025-05-23 12:21:27 -0400

    05-23-25 changes

--- config/zsh/src/modules/work.zsh
+++ config/zsh/src/modules/work.zsh
@@ -2,3 +2,13 @@
 
 export WORK_IP="$(cat ~/.env/WORK_IP)"
 export PROD_IP="$(cat ~/.env/PROD_IP)"
+export PROD_USER="$(cat ~/.env/PROD_USER)"
+
+srvcmd() {
+	ssh "$USER"@"$WORK_IP" "$*"
+}
+
+prodcmd() {
+	local cmd=$(printf '%q ' "$@")
+	srvcmd "ssh $PROD_USER@$PROD_IP $cmd"
+}
